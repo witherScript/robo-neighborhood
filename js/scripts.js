fecfda8b42dc2eb61949d8e82cb2312743833e0a
Genesis Scott <chandlerscott123@gmail.com>
2023-06-30 17:36:09 -0500

    format JS to contain UI logice, display array through forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,11 @@
 /*
-Test: "It should return an array of numbers from 0 to the user's inputted number"
-Code: beepBoop(0);
-Expected Output: [0]
+Business Logic
 */
 
 function getArray(number) {
 
   const numArr = new Array(number + 1);
-  for (let index = 0; index < numArr.length + 1; index++) {
+  for (let index = 0; index < numArr.length; index++) {
     if (index.toString().includes('3')) {
       numArr[index] = "Won't you be my neighbor?";
     }
@@ -17,6 +15,68 @@ function getArray(number) {
     else if (index.toString().includes('1')) {
       numArr[index] = "Beep!";
     }
-
+    else {
+      numArr[index] = index;
+    }
   }
+  return numArr;
+}
+
+/* 
+UI Logic 
+*/
+
+function hideElement(element) {
+  element.classList.add('hidden');
+}
+
+function unhideElement(element) {
+  element.classList.remove('hidden');
+}
+
+function displayArray(array) {
+
+  const divResults = document.getElementById('results');
+  divResults.innerHTML = '';
+  console.log(array);
+  array.forEach((element, index) => {
+    const p = document.createElement('p');
+    p.textContent = 'Index: ' + index + ', Value: ' + element;
+    p.id = 'elementNum-' + (index + 1).toString();
+    divResults.appendChild(p);
+
+  });
+
+  unhideElement(divResults);
+  unhideElement(document.getElementById('reset'));
+
+}
+
+function resetForm() {
+  const form = document.getElementById('number-form');
+  const resultsDiv = document.getElementById('results');
+  const resetButton = document.getElementById('reset');
+  hideElement(resultsDiv);
+  hideElement(resetButton);
+  unhideElement(form);
+  form.reset();
+}
+
+function handleSubmission(event) {
+  event.preventDefault();
+  const form = document.getElementById('number-form');
+  const input = document.querySelector('input#user-number').value;
+  const array = getArray(parseInt(input));
+  const resetButton = document.getElementById('reset');
+  displayArray(array);
+  hideElement(form);
+
+  resetButton.addEventListener('click', resetForm);
+}
+
+window.addEventListener('load', handleUserInput);
+
+function handleUserInput() {
+  const form = document.getElementById('number-form');
+  form.addEventListener('submit', handleSubmission);
 }
