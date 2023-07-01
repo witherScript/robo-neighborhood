95632b249cfdc7204bcc0b68bd47812b4b441560
Genesis Scott <chandlerscott123@gmail.com>
2023-07-01 13:27:38 -0500

    add error handling for numbers greater than 1000

--- js/scripts.js
+++ js/scripts.js
@@ -46,7 +46,37 @@ function toCardElement(element, index) {
 
   const cardHeader = document.createElement('div');
   cardHeader.classList.add('card-header');
-  cardHeader.textContent = 'Array value at location ' + index;
+
+  const cardHeaderTitle = document.createElement('i');
+  cardHeaderTitle.classList.add('fa-solid');
+
+  if (index < 10) {
+    cardHeaderTitle.classList.add(`fa-${index}`);
+  }
+  else if (index < 100) {
+    const firstDigitIcon = document.createElement('i');
+    firstDigitIcon.classList.add('fa-solid', `fa-${index.toString()[0]}`);
+    const secondDigitIcon = document.createElement('i');
+    secondDigitIcon.classList.add('fa-solid', `fa-${index.toString()[1]}`);
+    cardHeaderTitle.appendChild(firstDigitIcon);
+    cardHeaderTitle.appendChild(secondDigitIcon);
+  }
+  else {
+
+    const firstDigitIcon = document.createElement('i');
+    firstDigitIcon.classList.add('fa-solid', `fa-${index.toString()[0]}`);
+    const secondDigitIcon = document.createElement('i');
+    secondDigitIcon.classList.add('fa-solid', `fa-${index.toString()[1]}`);
+    const thirdDigitIcon = document.createElement('i');
+    thirdDigitIcon.classList.add('fa-solid', `fa-${index.toString()[2]}`);
+
+    cardHeaderTitle.appendChild(firstDigitIcon);
+    cardHeaderTitle.appendChild(secondDigitIcon);
+    cardHeaderTitle.appendChild(thirdDigitIcon);
+  }
+
+  cardHeader.append(cardHeaderTitle);
+
 
   const cardBody = document.createElement('div');
   cardBody.classList.add('card-body', 'text-primary');
@@ -70,18 +100,64 @@ function toCardElement(element, index) {
 
 }
 
+/*
+utility function to handle array length greater than 1000
+*/
+
+function handleArrayGreaterThan1000(array) {
+  const cardDiv = document.createElement('div');
+  cardDiv.classList.add('card', 'border-primary', 'mb-3');
+  cardDiv.style.maxWidth = '18rem';
+
+  const cardHeader = document.createElement('div');
+  cardHeader.classList.add('card-header');
+
+  const cardHeaderTitle = document.createElement('i');
+  cardHeaderTitle.classList.add('fa-circle-exclamation');
+  cardHeaderTitle.classList.add('fa-solid');
+  cardHeaderTitle.style.color = 'red';
+  cardHeaderTitle.style.fontSize = '2rem';
+  cardHeaderTitle.style.onmouseover = 'this.style.color = "blue"';
+  cardHeaderTitle.style.onmouseout = 'this.style.color = "red"';
+  cardHeaderTitle.style.cursor = 'help';
+  cardHeader.append(cardHeaderTitle);
+
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body', 'text-primary');
+
+
+  const cardText = document.createElement('p');
+  cardText.classList.add('card-text');
+  cardText.textContent = 'Number too large to display. Please enter a number less than 1000.';
+
+
+  cardBody.appendChild(cardText);
+  cardDiv.appendChild(cardHeader);
+  cardDiv.appendChild(cardBody);
+  document.getElementById('results').appendChild(cardDiv);
+  unhideElement(document.getElementById('results'));
+  unhideElement(document.getElementById('reset'));
+  document.getElementById('reset').addEventListener('click', resetForm);
+
+}
+
 function displayArray(array) {
 
   const divResults = document.getElementById('results');
   divResults.innerHTML = '';
-  array.forEach((element, index) => {
+  if (array.length > 1000) {
+    handleArrayGreaterThan1000(array);
+  }
+  else {
+    array.forEach((element, index) => {
 
-    divResults.appendChild(toCardElement(element, index));
+      divResults.appendChild(toCardElement(element, index));
 
-  });
+    });
 
-  unhideElement(divResults);
-  unhideElement(document.getElementById('reset'));
+    unhideElement(divResults);
+    unhideElement(document.getElementById('reset'));
+  }
 
 }
 
